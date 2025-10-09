6fdc689e35f2f430ede3fd7380edb563f4c1e231
Ashwani4011 <ashwanisharma6804@gmail.com>
2025-10-09 12:09:39 +0530

    Updated fetchNews.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"; 
-import { fetchNews } from "./logic"; 
+import { fetchNews } from "./Components/Fetchnews"; 
 import './App.css';
 import Card from "./Components/Card"
 import Navbar from './Components/Navbar';
--- src/Components/Fetchnews.jsx
+++ src/Components/Fetchnews.jsx
@@ -1,3 +1,4 @@
+// src/fetchNews.jsx
 
 const API_KEY = "b40ba3201745418b83c2d7c816c9d167";
 const BASE_URL = "https://newsapi.org/v2/everything?q=";
@@ -5,5 +6,5 @@ const BASE_URL = "https://newsapi.org/v2/everything?q=";
 export async function fetchNews(query = "India") {
   const res = await fetch(`${BASE_URL}${query}&apiKey=${API_KEY}`);
   const data = await res.json();
-  return data.articles.filter(article => article.urlToImage);
+  return data.articles.filter((article) => article.urlToImage);
 }
